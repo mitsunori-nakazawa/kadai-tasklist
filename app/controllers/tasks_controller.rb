21b39c6d087f581a7722b5c50daadee6c14a3319
mitsunori.nakazawa <mtnr.lions@gmail.com>
2021-02-07 07:56:08 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,38 +1,41 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
+    before_action :correct_user, only:[:show,:update,:edit,:destroy]
+    
   def index
-    @tasks = Task.all.page(params[:page]).per(5)
+    @tasks=current_user.tasks.order(id: :desc).page(params[:page])
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  # def show
+  # @task=current_user.tasks.build
+  # end
 
   def new
-    @task = Task.new
+    @task=current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task=current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  # def edit
+  #   @task = Task.find(params[:id])
+  # end
 
   def update
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
@@ -53,6 +56,13 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task=current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
 end
